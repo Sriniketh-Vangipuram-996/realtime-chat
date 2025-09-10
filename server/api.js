6821d62207547e3177b3c19a290c92f9944c2a5a
Sriniketh <srinikethvangipuram996@gmail.com>
2025-09-10 23:26:42 +0530

    feat:setup serverless api for vercel

--- server/api.js
+++ server/api.js
@@ -1,7 +1,4 @@
-const app=require('./index.js');
+const app=require('./index');
 const serverless=require('serverless-http');
 
-module.exports=(req,res)=>{
-    const handler=serverless(app);
-    return handler(req,res);
-}
+module.exports.handler=serverless(app);
